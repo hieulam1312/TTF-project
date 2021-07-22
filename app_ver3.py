2d410b055d5e24d196a77f32b11c24503681cfd3
hieulam1312 <hieulam1312@gmail.com>
2021-07-22 08:55:03 +0700

    Update app_ver3.py

--- app_ver3.py
+++ app_ver3.py
@@ -12,7 +12,7 @@ from datetime import datetime, timedelta
 from datetime import datetime as dt
 from typing import Text
 from oauth2client.service_account import ServiceAccountCredentials #-> Để nhập Google Spreadsheet Credentials
-
+import waterfall_chart
 from numpy.core.numeric import NaN
 import streamlit as st
 import json
@@ -39,7 +39,7 @@ spreadsheet_key = '1ECQkxew8ixIxb43FVyd3d8LmtRo5VHr1fvt3A7a88L8'
 sh9 = gc3.open("MẪU - dataset for Python").worksheet('Error')
 error_=sh9.get_all_records()
 error_df=pd.DataFrame(error_)
-
+error_df=error_df[['SỐ_ĐƠN_HÀNG','BƯỚC','NHÀ_MÁY','TÌNH_TRẠNG','BỘ_PHẬN','NGÀY_NHẬN','NGÀY_GIAO','NGÀY_GIẢI_QUYẾT','NHÓM_MẪU']]
 sh4=gc2.open('TTF - MẪU 2021 - TRIỂN KHAI').worksheet('D.SÁCH')
 order_df=sh4.get_all_records()
 order_df=pd.DataFrame(order_df)
@@ -52,9 +52,11 @@ sheet10=gc3.open("MẪU - dataset for Python").worksheet('TD')
 process_=sheet10.get_all_records()
 process_df=pd.DataFrame(process_)
 process_df.columns=process_df.columns.str.replace(' ',"_")
+process_df=process_df.replace("",np.nan)
 plan_df.columns=plan_df.columns.str.replace(' ',"_")
 attend_=error_df.merge(order_df,how='left',on='SỐ_ĐƠN_HÀNG')
-attend_df=attend_[['SỐ_ĐƠN_HÀNG','BƯỚC','MÃ_KHÁCH_HÀNG','NV_PTM_y','TÊN_SẢN_PHẨM_y','NHÀ_MÁY_x','NVLM','TÌNH_TRẠNG_x','BỘ_PHẬN','NGÀY_NHẬN','NGÀY_GIAO_x','NGÀY_GIẢI_QUYẾT','NHÓM_MẪU']]
+error_all=error_df.merge(order_df,how='left',on='SỐ_ĐƠN_HÀNG')
+attend_df=attend_[['SỐ_ĐƠN_HÀNG','BƯỚC','MÃ_KHÁCH_HÀNG','NV_PTM','TÊN_SẢN_PHẨM','NHÀ_MÁY_x','TÌNH_TRẠNG_x','BỘ_PHẬN','NGÀY_NHẬN','NGÀY_GIAO_x','NGÀY_GIẢI_QUYẾT','NHÓM_MẪU']]
 conditions = [
     (attend_df['BƯỚC'] <= 3),(attend_df['BƯỚC'] == 5),(attend_df['BƯỚC'] == 6),(attend_df['BƯỚC'] ==7),(attend_df['BƯỚC'] ==8),(attend_df['BƯỚC'] ==9),(attend_df['BƯỚC'] ==10),
     (attend_df['BƯỚC'] ==11),(attend_df['BƯỚC'] >11)]
@@ -119,7 +121,7 @@ def check_plan(plan):
     st.markdown("""
     ### A. DANH SÁCH KẾ HOẠCH MẪU
     """)
-    col1,col2=st.beta_columns((1,1))
+    c,col1,e,col2,d=st.beta_columns((.5,2,.2,2,.5))
     with col1:
         plan_today=plan_toweek.loc[plan_toweek.REMARKS=="HÔM NAY"]
         plan_today=plan_today.reset_index(drop=True)
@@ -127,7 +129,18 @@ def check_plan(plan):
         st.markdown('')    
         st.write(plan_today)
         st.markdown('')
-
+        plan_late=plan_toweek.loc[plan_toweek.REMARKS=="TRỄ"]
+        plan_late=plan_late.reset_index(drop=True)
+        st.markdown("<h4 style='text-align: left; color:red'>ĐANG TRỄ </h4>", unsafe_allow_html=True)
+        st.markdown('')  
+        st.write(plan_late)
+        st.markdown('')
+        st.markdown("<h4 style='text-align: left'>TRONG TUẦN</h4>", unsafe_allow_html=True)
+        st.markdown('')    
+        plan_doing=plan_toweek.loc[(plan_toweek.REMARKS=='ĐANG LÀM')]
+        st.write(plan_doing)
+        st.markdown("")
+        st.markdown("")
     #Plan ngày mai
     with col2:
         plan_tomorrow=plan_toweek.loc[plan_toweek.REMARKS=="NGÀY MAI"]
@@ -137,23 +150,10 @@ def check_plan(plan):
         st.write(plan_tomorrow)
         st.markdown('')
     #plan đang bị trễ
-        plan_late=plan_toweek.loc[plan_toweek.REMARKS=="TRỄ"]
-        plan_late=plan_late.reset_index(drop=True)
-        st.markdown("<h4 style='text-align: left; color:red'>ĐANG TRỄ </h4>", unsafe_allow_html=True)
-        st.markdown('')  
-        st.write(plan_late)
 
         st.markdown("<h4 style='text-align: left; color:blue'>ĐÃ GIAO HÀNG TRẮNG</h4>", unsafe_allow_html=True)
         st.markdown('')  
         st.write(plan_done)
-        st.markdown('')
-    st.markdown("<h4 style='text-align: left'>TRONG TUẦN</h4>", unsafe_allow_html=True)
-    st.markdown('')    
-    plan_doing=plan_toweek.loc[(plan_toweek.REMARKS=='ĐANG LÀM')]
-    st.write(plan_doing)
-    st.markdown("")
-    st.markdown("")
-
 def check_order(id):
     hist_order=order_df.loc[order_df.SỐ_ĐƠN_HÀNG==id]
     hist_orderr=hist_df.loc[hist_df.SỐ_ĐƠN_HÀNG==id].reset_index(drop=True)
@@ -164,64 +164,123 @@ def check_order(id):
     st.markdown('MÃ_KHÁCH_HÀNG: **{}**'.format(product_name[0][3]))
     historder=hist_orderr[['BỘ_PHẬN','NGÀY_NHẬN','NGÀY_GIAO_x','NGÀY_GIẢI_QUYẾT']]
     st.dataframe(data=historder, width=700, height=1068)
-
 def check_error(error):
-    col1,col2=st.beta_columns((1,1))
+    c,col1,e,col2,d=st.beta_columns((.5,2,.2,2,.5))
     with col1:
         st.markdown('Chưa scan giao')
-        out=error.loc[error['TÌNH_TRẠNG']=='Chưa giao']
-        out_df=out[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM','NHÀ_MÁY']]
+        out=error.loc[error['TÌNH_TRẠNG_x']=='Chưa giao']
+        out_df=out[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM','NHÀ_MÁY_x']]
         out_df
         st.markdown('Chưa scan nhận')
-        in_=error.loc[error['TÌNH_TRẠNG']=='Chưa nhận']
-        in_df=in_[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM','NHÀ_MÁY']]
+        in_=error.loc[error['TÌNH_TRẠNG_x']=='Chưa nhận']
+        in_df=in_[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM','NHÀ_MÁY_x']]
         in_df
     with col2:
         st.markdown('Đang xử lí')
-        doing=error.loc[error['TÌNH_TRẠNG']=='Đang xử lí']
-        doing_df=doing[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM','NHÀ_MÁY']] 
+        doing=error.loc[error['TÌNH_TRẠNG_x']=='Đang xử lí']
+        doing_df=doing[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM','NHÀ_MÁY_x']] 
         doing_df
+def operation(df,bp):
+    c,col1,d,col2,e=st.beta_columns((.5,10,.2,8,.5))
+    with col1:
+        df_=df.loc[df['TÌNH_TRẠNG_x']!='Chưa giao']
+        doing_count=df_.groupby(df_['BỘ_PHẬN']).SỐ_ĐƠN_HÀNG.count()
+        doing_count=doing_count.reset_index()
+        fig1, ax = plt.subplots()   
+        plt.barh(doing_count['BỘ_PHẬN'], doing_count['SỐ_ĐƠN_HÀNG'], align='center')
+        # plt.xlabel(doing_count['BỘ_PHẬN'])
+        # plt.ylabel(doing_count['SỐ_ĐƠN_HÀNG'])
+        plt.show()
+    c,col1,d,col2,e=st.beta_columns((.5,10,.2,8,.5))
+    with col1:
+        st.pyplot(fig1)
+        
+    st.markdown('')
+    nm=df_.drop_duplicates(subset=['SỐ_ĐƠN_HÀNG'])
+    NM=nm.groupby(nm['NHÀ_MÁY_x']).SỐ_ĐƠN_HÀNG.count()
+    NM_df=NM.reset_index()
+    NM_df['NHÀ_MÁY_x']=NM_df['NHÀ_MÁY_x'].str.replace('#N/A','Chưa phân bổ')
+    fig, ax = plt.subplots()   
+    sns.barplot(data=NM_df,x=NM_df['NHÀ_MÁY_x'],y=NM_df['SỐ_ĐƠN_HÀNG'])
+    with col2:
+        st.pyplot(fig)
+
+    time=bp.loc[(bp['NGÀY_GIẢI_QUYẾT'].isnull==False)+(bp['NGÀY_GIẢI_QUYẾT']<1000)]
+    time_df=time[['SỐ_ĐƠN_HÀNG','BỘ_PHẬN','NGÀY_NHẬN','NGÀY_GIAO','NGÀY_GIẢI_QUYẾT','NHÓM_MẪU']]
+    time_df['NGÀY_GIAO']=pd.to_datetime(time_df['NGÀY_GIAO'])
+    time_df['THÁNG_GIAO']=time_df['NGÀY_GIAO'].dt.month
+    water_df=time_df.groupby(['BỘ_PHẬN','THÁNG_GIAO']).mean()
+    water_df=water_df.reset_index()
+
+
+    r1,r2,r3,r4,r5=st.beta_columns((.5,.5,.1,1,.5))
+    with r2:
+        month=st.number_input('Nhập tháng để xem:',step=1)
+    h,c1,d=st.beta_columns((.5,2,.5))
+    with c1:
+        if not month:
+            avg=water_df.groupby('BỘ_PHẬN').mean()
+            avg=avg.reset_index()
+        else:
+            m_water=water_df.loc[water_df['THÁNG_GIAO']==month]
+            avg=m_water.groupby('BỘ_PHẬN').mean()
+            avg=avg.reset_index()  
+        st.markdown('## THỜI GIAN XỬ LÍ TRUNG BÌNH CỦA CÁC BỘ PHẬN')
+        waterfall_chart.plot(avg['BỘ_PHẬN'], avg["NGÀY_GIẢI_QUYẾT"],rotation_value=70)
+        st.pyplot()
 ########################################
+
 # st.set_option('deprecation.showPyplotGlobalUse', False)
 col1 = st.sidebar
-choose=col1.selectbox('Chọn đối tượng 1',['NHÀ_MÁY','NV_PTM','BỘ_PHẬN','SỐ_ĐƠN_HÀNG'])
-if choose=='NV_PTM':
-    _1 = ["A. Hoàng","A. Sáng",'A. Bảo','C. Hai','C. Như','C. Thy']
-    choose_type=col1.selectbox('Chọn đối tượng 2',_1)
-    c=st.sidebar.selectbox('Chọn',['VỊ TRÍ CỦA MẪU','KẾ HOẠCH MẪU TUẦN NÀY'])
-    if c=='VỊ TRÍ CỦA MẪU':
-        check_by_per=attend_df.loc[attend_df['NV_PTM_y']==choose_type]
-        check_by_per=check_by_per[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM_y','NHÀ_MÁY_x','TÌNH_TRẠNG_x','VỊ TRÍ']]
-        check_attend(check_by_per)
-    else:
-        plan_=plan_df.merge(order_df,how='left',on='SỐ_ĐƠN_HÀNG')
-        plan_=plan_[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM_x','NGÀY_KẾ_HOẠCH','REMARKS','NHÀ_MÁY','NV_PTM','WEEK']]
-        check_by_per=plan_.loc[plan_['NV_PTM']==choose_type]
-        check_plan(check_by_per)
-
-elif choose=="NHÀ_MÁY":
-    _1= ['NM1','NM3','X4','NM NỆM']
-    choose_type=col1.selectbox('Chọn đối tượng 2',_1)
-    check_by_per=attend_df.loc[attend_df['NHÀ_MÁY_x']==choose_type]
-    check_by_per=check_by_per[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM_y','NHÀ_MÁY_x','TÌNH_TRẠNG_x','VỊ TRÍ']]
-    
-    c=st.sidebar.selectbox('Chọn',['VỊ TRÍ CỦA MẪU','KẾ HOẠCH MẪU TUẦN NÀY'])
-    if c=='VỊ TRÍ CỦA MẪU':
-        check_attend(check_by_per)
-    else:
-        plan_=plan_df.merge(order_df,how='left',on='SỐ_ĐƠN_HÀNG')
-        plan_=plan_[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM_x','NGÀY_KẾ_HOẠCH','REMARKS','NHÀ_MÁY','NV_PTM','WEEK']]
-        check_by_per=plan_.loc[plan_['NHÀ_MÁY']==choose_type]
-        check_plan(check_by_per)
-elif  choose=='SỐ_ĐƠN_HÀNG':
-    choose_type=st.sidebar.text_input('Nhập tên đơn hàng','M.00.00.00')
-    if not choose_type:
-        st.error('Hãy nhập mã đơn hàng!')
-    else:
-        check_order(choose_type)
+t1,t2,t3,t4,t5=st.beta_columns((5,.5,.1,1,.5))
+with t1:
+    st.title('OPERATION DASHBOARD')
+r1,r2,r3,r4,r5=st.beta_columns((.5,.5,.1,1,.5))
+with r1:
+    ch=st.selectbox('',['OVERVIEW','KIỂM TRA TIẾN ĐỘ'])
+if ch=='OVERVIEW':
+    st.markdown('### OVERVIEW')
+    st.markdown('Danh sách mẫu tại mỗi bộ phận')
+    operation(error_all,process_df)
 else:
-    _1=process_df['BỘ_PHẬN'].unique().tolist()
-    choose_type=col1.selectbox('Chọn đối tượng 2',_1)
-    _error=error_df.loc[error_df['BỘ_PHẬN']==choose_type]
-    check_error(_error)
+    choose=col1.selectbox('Chọn đối tượng 1',['NHÀ_MÁY','NV_PTM','BỘ_PHẬN','SỐ_ĐƠN_HÀNG'])
+    if choose=='NV_PTM':
+        _1 = ["A. Hoàng","A. Sáng",'A. Bảo','C. Hai','C. Như','C. Thy']
+        choose_type=col1.selectbox('Chọn đối tượng 2',_1)
+        c=st.sidebar.selectbox('Chọn',['VỊ TRÍ CỦA MẪU','KẾ HOẠCH MẪU TUẦN NÀY'])
+        if c=='VỊ TRÍ CỦA MẪU':
+            check_by_per=attend_df.loc[attend_df['NV_PTM_y']==choose_type]
+            check_by_per=check_by_per[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM_y','NHÀ_MÁY_x','TÌNH_TRẠNG_x','VỊ TRÍ']]
+            check_attend(check_by_per)
+        else:
+            plan_=plan_df.merge(order_df,how='left',on='SỐ_ĐƠN_HÀNG')
+            plan_=plan_[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM_x','NGÀY_KẾ_HOẠCH','REMARKS','NHÀ_MÁY','NV_PTM','WEEK']]
+            check_by_per=plan_.loc[plan_['NV_PTM']==choose_type]
+            check_plan(check_by_per)
+
+    elif choose=="NHÀ_MÁY":
+        _1= ['NM1','NM3','X4','NM NỆM']
+        choose_type=col1.selectbox('Chọn đối tượng 2',_1)
+        check_by_per=attend_df.loc[attend_df['NHÀ_MÁY_x']==choose_type]
+        check_by_per=check_by_per[['SỐ_ĐƠN_HÀNG','MÃ_KHÁCH_HÀNG','TÊN_SẢN_PHẨM','NHÀ_MÁY_x','TÌNH_TRẠNG_x','VỊ TRÍ']]
+        
+        c=st.sidebar.selectbox('',['KẾ HOẠCH MẪU TUẦN NÀY','VỊ TRÍ CỦA MẪU'])
+        if c=='VỊ TRÍ CỦA MẪU':
+            check_attend(check_by_per)
+        else:
+            plan_=plan_df.merge(order_df,how='left',on='SỐ_ĐƠN_HÀNG')
+            plan_=plan_[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM_x','NGÀY_KẾ_HOẠCH','REMARKS','NHÀ_MÁY','NV_PTM','WEEK']]
+            check_by_per=plan_.loc[plan_['NHÀ_MÁY']==choose_type]
+            check_plan(check_by_per)
+    elif  choose=='SỐ_ĐƠN_HÀNG':
+        choose_type=st.sidebar.text_input('Nhập tên đơn hàng','M.00.00.00')
+        if not choose_type:
+            st.error('Hãy nhập mã đơn hàng!')
+        else:
+            check_order(choose_type)
+    else:
+        _1=process_df['BỘ_PHẬN'].unique().tolist()
+        choose_type=col1.selectbox('Chọn đối tượng 2',_1)
+        _error=error_all.loc[error_df['BỘ_PHẬN']==choose_type]
+        check_error(_error)
 
