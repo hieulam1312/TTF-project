5d3a5be56dcc9731c37cced41f4ac3662e45bdb4
hieulam1312 <83022199+hieulam1312@users.noreply.github.com>
2021-07-23 13:35:14 +0700

    Update app_ver3.py

--- app_ver3.py
+++ app_ver3.py
@@ -13,11 +13,12 @@ from datetime import datetime, timedelta,date
 from datetime import datetime as dt
 from typing import Text
 from oauth2client.service_account import ServiceAccountCredentials #-> Để nhập Google Spreadsheet Credentials
-import waterfall_chart
+# import waterfall_chart
 from numpy.core.numeric import NaN
 import streamlit as st
 import json
 import requests
+import altair as alt
 pd.plotting.register_matplotlib_converters()
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -31,6 +32,9 @@ credentials = service_account.Credentials.from_service_account_info(
     scopes=['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive'],
 )
+
+gc1 = gspread.authorize(credentials)
+spreadsheet_key = '1pMA3ZDjkL8gtIzQMyEkHwL_8-wDrXiO45OgtsPrkFec'
 gc2 = gspread.authorize(credentials)
 spreadsheet_key='1Kf79UeBTa0q2NAh4PaW2Y1nqE__S0wiSQSOkk2dkQm0'
 
@@ -48,22 +52,93 @@ order_df=pd.DataFrame(order_df)
 sh5=gc2.open('TTF - MẪU 2021 - TRIỂN KHAI').worksheet('T.ĐỘ SX')
 plan_df=sh5.get_all_records()
 plan_df=pd.DataFrame(plan_df)
-order_df.columns = order_df.columns.str.replace(' ', '_')
-sheet10=gc3.open("MẪU - dataset for Python").worksheet('TD')
-process_=sheet10.get_all_records()
-process_df=pd.DataFrame(process_)
-process_df.columns=process_df.columns.str.replace(' ',"_")
-process_df=process_df.replace("",np.nan)
+# order_df.columns = order_df.columns.str.replace(' ', '_')
 
+#scan_df
+sh1 = gc1.open("MẪU 2021 - COLLECT DATA").worksheet('UNPIVOT')
+scan_df=sh1.get_all_records()
+scan_df=pd.DataFrame(scan_df)
+#td_df
+sh2=gc1.open("MẪU 2021 - COLLECT DATA").worksheet('TD')
+td_df=sh2.get_all_records()
+td_df=pd.DataFrame(td_df)
+#td_x4_df
+sh3=gc1.open("MẪU 2021 - COLLECT DATA").worksheet('TD X4-NỆM')
+td_x4_df=sh3.get_all_records()
+td_x4_df=pd.DataFrame(td_x4_df)
+sh6=gc1.open("MẪU 2021 - COLLECT DATA").worksheet('XL+ SL')
+xl_sl=sh6.get_all_records()
+xl_sl_df=pd.DataFrame(xl_sl)
+#order_df
+
+sh5=gc2.open('TTF - MẪU 2021 - TRIỂN KHAI').worksheet('T.DÕI')
+td_old=sh5.get_all_records()
+td_old_df=pd.DataFrame(td_old)
+
+sh7=gc3.open('TTF - MẪU 2021 - DƯỚI 12').worksheet('D.SÁCH')
+under_12ds=sh7.get_all_records()
+under_12ds_df=pd.DataFrame(under_12ds)
+sh8=gc3.open('TTF - MẪU 2021 - DƯỚI 12').worksheet('T.DÕI')
+under_12td=sh8.get_all_records()
+under_12td_df=pd.DataFrame(under_12td)
+
+sh8=gc2.open('TTF - MẪU 2021 - TRIỂN KHAI').worksheet('Sheet53')
+dataaa=sh8.get_all_records()
+data=pd.DataFrame(dataaa)
+
+nm_df=td_df.loc[(td_df['NHÀ MÁY']!='X4')|(td_df['NHÀ MÁY']!='NM NỆM')]
+td_new_df=pd.concat([nm_df,td_x4_df])
+td_new_df=td_new_df[['SỐ ĐƠN HÀNG','BƯỚC','IN','OT','NHÀ MÁY','NMVLM','BỘ PHẬN','NGÀY GIẢI QUYẾT','NHÓM MẪU']]
+td_new_df=td_new_df.rename(columns={'IN': 'NGÀY NHẬN','OT':'NGÀY GIAO','NMVLM':'NVLM'})
+
+# td_old_df.columns=td_old_df.columns.str.replace(" ","_")
+td_all_df=pd.concat([td_old_df,td_new_df,under_12td_df])
+td_all_df=td_all_df.replace('',np.nan)
+td_2021_df=td_all_df[td_all_df['SỐ ĐƠN HÀNG'].notnull()]
+td_2021_df.head()
+
+# under_12ds_df=under_12ds_df.rename(columns={'NV LÀM MẪU':'NVLM'})
+under_12ds_df=under_12ds_df.drop(['BƯỚC'],axis=1)
+nvlm=td_all_df
+nvlm=nvlm[['SỐ ĐƠN HÀNG','NVLM']]
+nvlm_df=nvlm.drop_duplicates()
+nvlm_df=nvlm_df.dropna()
+
+# xl_sl_df.columns=xl_sl_df.columns.str.replace(' ','_')
+xl=xl_sl_df.loc[xl_sl_df['THAO TÁC']==' Giao đơn hàng']
+xl_df=xl[['SỐ ĐH','XẾP LOẠI','SL THỰC TẾ']]
+xl_df=xl_df.rename(columns={'SỐ ĐH':'SỐ ĐƠN HÀNG'})
+
+
+under_12ds_df=under_12ds_df.drop(['NV LÀM MẪU'],axis=1)
+order_df=order_df.drop(['GỖ','SƠN','NỆM','BAO BÌ','GHI CHÚ','HÌNH ẢNH'], axis = 1)
+new_order=order_df.merge(xl_df,how='left',on='SỐ ĐƠN HÀNG')
+
+order_2021_df=pd.concat([new_order,under_12ds_df])
+order_new=order_2021_df.merge(nvlm_df,how='left',on='SỐ ĐƠN HÀNG')
+order_new=order_new.replace(np.nan,0)
+order_new['NGÀY GIAO']
+pd.to_datetime(order_new['NGÀY GIAO'],dayfirst=True,errors='coerce')
+# order_2021_df['NGÀY GIAO'].astype('datetime64')
+# df['date'].astype('datetime64')
+order_new['NGÀY GIAO']=pd.to_datetime(order_new['NGÀY GIAO'],dayfirst=True,errors='coerce')
+order_new['NGÀY LẬP']=pd.to_datetime(order_new['NGÀY LẬP'],dayfirst=True,errors='coerce')
+td_2021_df['NGÀY NHẬN']=pd.to_datetime(td_2021_df['NGÀY NHẬN'],dayfirst=True,errors='coerce')
+td_2021_df['NGÀY GIAO']=pd.to_datetime(td_2021_df['NGÀY GIAO'],dayfirst=True,errors='coerce')
 sheet11=gc3.open("MẪU - dataset for Python").worksheet('CALC')
 calc_=sheet11.get_all_records()
 calc_df=pd.DataFrame(calc_)
-calc_df=calc_df[['SỐ ĐƠN HÀNG','TÊN SẢN PHẨM','NHÀ MÁY','NVLM','NGÀY NVLM GIAO','THÁNG GIAO','T/G TTF']]
+calc_df=calc_df[['SỐ ĐƠN HÀNG','TÊN SẢN PHẨM','NHÀ MÁY','NVLM','NGÀY NVLM GIAO','THÁNG GIAO','TUẦN GIAO','T/G TTF']]
 calc_df['NGÀY NVLM GIAO']=pd.to_datetime(calc_df['NGÀY NVLM GIAO'])
-calc_df['TUẦN GIAO']=calc_df['NGÀY NVLM GIAO'].dt.week
+
 calc_df.columns=calc_df.columns.str.replace(' ',"_")
 calc_df=calc_df.replace("",np.nan)
-###
+
+process_df=td_2021_df
+process_df.columns=process_df.columns.str.replace(' ',"_")
+order_2021_df
+
+
 plan_df.columns=plan_df.columns.str.replace(' ',"_")
 attend_=error_df.merge(order_df,how='left',on='SỐ_ĐƠN_HÀNG')
 error_all=error_df.merge(order_df,how='left',on='SỐ_ĐƠN_HÀNG')
@@ -201,6 +276,7 @@ def operation(df,bp,calc,plan):
     done_['T/G_TTF']=done_['T/G_TTF'].astype(float)
     time_month=done_.loc[done_['T/G_TTF'].isnull()==False]
     avg_month=time_month['T/G_TTF'].mean()
+    done=calc.groupby(['TUẦN_GIAO','NVLM']).SỐ_ĐƠN_HÀNG.count().reset_index()
 
     done_w=calc.loc[calc['TUẦN_GIAO']==week_]
     done_week=done_w.groupby(['NHÀ_MÁY','NVLM']).SỐ_ĐƠN_HÀNG.count().reset_index()
@@ -212,7 +288,6 @@ def operation(df,bp,calc,plan):
     _1,_2,_3,_4,_5=st.beta_columns((.5,10,.2,10,.5))
     fig3, ax = plt.subplots()   
     sns.set_palette("pastel")
-
     st.set_option('deprecation.showPyplotGlobalUse',False)
     sns.barplot(data=done_month,x=done_month['NVLM'],y=done_month['SỐ_ĐƠN_HÀNG'],color='Green')
     plt.xticks(rotation=90)
@@ -220,7 +295,7 @@ def operation(df,bp,calc,plan):
     fig4, ax = plt.subplots()   
     st.set_option('deprecation.showPyplotGlobalUse',False)
     sns.set_palette("pastel")
-    sns.barplot(data=done_week,x=done_week['NVLM'],y=done_week['SỐ_ĐƠN_HÀNG'])
+    sns.barplot(data=done_week,x=done_week['NVLM'],y=done_week['SỐ_ĐƠN_HÀNG'],color='Blue')
     plt.xticks(rotation=90)
     plt.show()
     with _2:
@@ -232,19 +307,18 @@ def operation(df,bp,calc,plan):
         st.markdown('Kết quả tuần: **{}**'.format(week_))
         st.markdown('Số lượng đã xong hàng trắng: **{}**'.format(total_week))
         st.markdown('Thời gian xử lí hàng trắng: **{}**'.format(avg_week))
-
         st.pyplot(fig4) 
-
-
-        plan_=plan.merge(calc,how='left',on='SỐ_ĐƠN_HÀNG')
-        plan_=plan_[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM_x','NGÀY_KẾ_HOẠCH','REMARKS','NHÀ_MÁY','WEEK']]
-        plan__=plan_.loc[plan_.WEEK==week_+1]
-        plan_toweek=plan__[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM_x','NHÀ_MÁY','REMARKS']]
-        plan_doing=plan__.loc[plan__.REMARKS!='Done']
-        plan_doing=plan_doing[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM_x','NHÀ_MÁY','REMARKS']]
-        plan_doing=plan_doing.reset_index(drop=True)
-        st.markdown("")
-        plan_doing
+    done_pivot=done.pivot(index='NVLM',columns='TUẦN_GIAO',values='SỐ_ĐƠN_HÀNG')
+    done_pivot
+    plan_=plan.merge(calc,how='left',on='SỐ_ĐƠN_HÀNG')
+    plan_=plan_[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM_x','NGÀY_KẾ_HOẠCH','REMARKS','NHÀ_MÁY','WEEK']]
+    plan__=plan_.loc[plan_.WEEK==week_+1]
+    plan_toweek=plan__[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM_x','NHÀ_MÁY','REMARKS']]
+    plan_doing=plan__.loc[plan__.REMARKS!='Done']
+    plan_doing=plan_doing[['SỐ_ĐƠN_HÀNG','TÊN_SẢN_PHẨM_x','NHÀ_MÁY','REMARKS']]
+    plan_doing=plan_doing.reset_index(drop=True)
+    st.markdown("")
+    plan_doing
 
     with col1:
         df_=df.loc[df['TÌNH_TRẠNG_x']!='Chưa giao']
@@ -293,7 +367,7 @@ def operation(df,bp,calc,plan):
         st.markdown('## THỜI GIAN XỬ LÍ TRUNG BÌNH CỦA CÁC BỘ PHẬN')
         waterfall_chart.plot(avg['BỘ_PHẬN'], avg["NGÀY_GIẢI_QUYẾT"],rotation_value=70)
         st.pyplot()
-########################################
+#######################################
 
 # st.set_option('deprecation.showPyplotGlobalUse', False)
 col1 = st.sidebar
