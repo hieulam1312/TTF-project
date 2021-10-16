811adef533f9b02bbe1c34d39291e29ec5b8116a
hieulam1312 <83022199+hieulam1312@users.noreply.github.com>
2021-10-16 10:20:44 +0700

    Create list_info.py

---
+++ Prediction/list_info.py
@@ -0,0 +1,35 @@
+ncc_list=["LEGAL (2C)",'LÒ SẤY 2.4 CHÚ BÉ (ABC)',
+'AN CƯỜNG',
+'BẢO HƯNG (2C)',
+'GỖ LAM (ABC)',
+'CHÂU MỸ (2C)',
+'MỘC BẢO GIA (BC)',
+'LÒ SẤY 2.3 CHÚ BÉ (ABC)',
+'LÒ SẤY 2.2 CHÚ BÉ (ABC)',
+'LÒ SẤY 2.7 CHÚ BÉ (ABC)',
+'LÒ SẤY 2.2 TÂN THÀNH',
+'LÒ SẤY 2.9 XUÂN TRUNG (ABC)',
+'LÒ SẤY 2.7 CHÚ BÉ  (ABC).',
+'LÒ SẤY 2.8 TÂN THÀNH (ABC)',
+'LÒ SẤY 2.8 XUÂN TRUNG (ABC)',
+'MỘC BẢO GIA (ABC)',
+'PHƯƠNG ĐÔNG (AB)',
+'S-WOOD (2C)',
+'TẤN TÀI ',
+'ĐÀO BÁCH',
+'AG WOOD GMBH (AA)',
+'AG WOOD GMBH (ABC)']
+
+
+go_list=['WHITE OAK',
+'CAO SU',
+'VÁN MDF',
+'WALNUT',
+'OAK',
+'BEECH',
+'TRÀM',
+'SỌ KHỈ',
+'VÁN MDF E2']
+
+
+qc_list=['Tuấn','Mứt','Trúc']
