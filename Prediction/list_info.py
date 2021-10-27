db5b64794b8842974014ca66293371533b819ef8
hieulam1312 <83022199+hieulam1312@users.noreply.github.com>
2021-10-27 08:37:40 +0700

    Update list_info.py

--- Prediction/list_info.py
+++ Prediction/list_info.py
@@ -20,16 +20,8 @@
 # 'AG WOOD GMBH (AA)',
 # 'AG WOOD GMBH (ABC)']
 
-
-go_list=['WHITE OAK',
-'CAO SU',
-'VÁN MDF',
-'WALNUT',
-'OAK',
-'BEECH',
-'TRÀM',
-'SỌ KHỈ',
-'VÁN MDF E2']
+intiatals=["WOK","CSU","MDF","WAL","OAK","BEE",'TRM',"SOK","THO",'VEP']
+go_list=['WHITE OAK','CAO SU','VÁN MDF','WALNUT','OAK','BEECH','TRÀM','SỌ KHỈ','THÔNG','VÁN ÉP']
 
 
 qc_list=['Tuấn','Mứt','Trúc']
