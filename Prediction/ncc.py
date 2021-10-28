06280fc18d9a4283fd8ce303095a40253d85eed6
hieulam1312 <83022199+hieulam1312@users.noreply.github.com>
2021-10-28 10:00:34 +0700

    Create ncc.py

---
+++ Prediction/ncc.py
@@ -0,0 +1,59 @@
+from gspread.utils import A1_ADDR_ROW_COL_RE
+
+
+def ncc_f():
+    import streamlit as st
+    import pandas as pd
+    from google.oauth2 import service_account
+    import gspread #-> Để update data lên Google Spreadsheet
+    from gspread_dataframe import set_with_dataframe #-> Để update data lên Google Spreadsheet
+    from oauth2client.service_account import ServiceAccountCredentials #-> Để nhập Google Spreadsheet Credentials
+    credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive'],
+    )
+    gc = gspread.authorize(credentials)
+    spreadsheet_key='1KBTVmlT5S2_x9VGseHdk_QDvZIfNBOLJy78lM0p3ORQ'
+
+    sh=gc.open('Kho NVL - NCC').worksheet('Sheet1')
+    sheet=sh.get_all_values()
+    ncc=pd.DataFrame(sheet)
+    ncc.columns=ncc.iloc[0]
+    ncc=ncc[1:]
+    # A = ncc
+    # B= ncc['MÃ'].unique().tolist()
+    return ncc
+# ncc_list=ncc()
+
+def push(df):
+    import streamlit as st
+    import pandas as pd
+    from google.oauth2 import service_account
+    import gspread #-> Để update data lên Google Spreadsheet
+    from gspread_dataframe import set_with_dataframe #-> Để update data lên Google Spreadsheet
+    from oauth2client.service_account import ServiceAccountCredentials #-> Để nhập Google Spreadsheet Credentials
+    credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive'],
+    )
+    gc = gspread.authorize(credentials)
+    spreadsheet_key='1KBTVmlT5S2_x9VGseHdk_QDvZIfNBOLJy78lM0p3ORQ'
+
+    sheet_index_no1= 8
+
+    sh = gc.open_by_key(spreadsheet_key)
+    worksheet1 = sh.get_worksheet(sheet_index_no1)#-> 0 - first sheet, 1 - second sheet etc. 
+
+    import gspread_dataframe as gd
+    import gspread as gs
+
+    ws = gc.open("Kho NVL - NCC").worksheet('Sheet2')
+    existing = gd.get_as_dataframe(ws)
+    updated = existing.append(df)
+    gd.set_with_dataframe(ws, updated)
+    st.success('Done')
+# LIST_=ncc_f()
+ncc_list=ncc_f()
+# initial_ncc=LIST_[1]
