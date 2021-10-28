ee34aed777d705e7dfa751ed392b9d6501a88153
hieulam1312 <83022199+hieulam1312@users.noreply.github.com>
2021-10-28 09:28:37 +0700

    Update cv.py

--- Prediction/cv.py
+++ Prediction/cv.py
@@ -22,6 +22,38 @@ def ncc():
     ncc.columns=ncc.iloc[0]
     ncc=ncc[1:]
     A = ncc['TÊN NCC'].unique().tolist()
-    return A
+    B= ncc['MÃ'].unique().tolist()
+    return A,B
 ncc_list=ncc()
-ncc_list
+
+def push(df):
+    import streamlit as st
+    import pandas as pd
+    from google.oauth2 import service_account
+    import gspread #-> Để update data lên Google Spreadsheet
+    from gspread_dataframe import set_with_dataframe #-> Để update data lên Google Spreadsheet
+    from oauth2client.service_account import ServiceAccountCredentials #-> Để nhập Google Spreadsheet Credentials
+    credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive'],
+    )
+    gc = gspread.authorize(credentials)
+    spreadsheet_key='1KBTVmlT5S2_x9VGseHdk_QDvZIfNBOLJy78lM0p3ORQ'
+
+    sheet_index_no1= 8
+
+    sh = gc.open_by_key(spreadsheet_key)
+    worksheet1 = sh.get_worksheet(sheet_index_no1)#-> 0 - first sheet, 1 - second sheet etc. 
+
+    import gspread_dataframe as gd
+    import gspread as gs
+
+    ws = gc.open("Kho NVL - NCC").worksheet('Sheet2')
+    existing = gd.get_as_dataframe(ws)
+    updated = existing.append(df)
+    gd.set_with_dataframe(ws, updated)
+    st.success('Done')
+LIST_=ncc()
+ncc_list=LIST_[0]
+initial_ncc=LIST_[1]
