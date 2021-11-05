a4df813767044e9d8004d8d1242ce83ba4eb701f
Hieu Thuan Lam <hieulam1312@gmail.com>
2021-11-05 13:48:57 +0700

    Update lsx_input_form.py

--- TTT_ver2/lsx_input_form.py
+++ TTT_ver2/lsx_input_form.py
@@ -19,38 +19,37 @@ import gspread as gs
 from gspread.utils import A1_ADDR_ROW_COL_RE
 def pull_lsx(gc):
     spreadsheet_key='1dUUWEBwnD4kSJAwI3Oi4_fXN1Yji8cdth4Rs2RewCuw'
-
     sh=gc.open('DSX1.1 - Master Đơn hàng').worksheet('MASTER DH')
     sheet=sh.get_all_values()
     ncc=pd.DataFrame(sheet)
     ncc.columns=ncc.iloc[0]
     ncc=ncc[1:]
     ncc["SỐ ĐƠN HÀNG"]=ncc["SỐ ĐH"]
-    # ncc["NMSX"]=""
-    # ncc["SẢN PHẨM (C/M)"]=""
-    # ncc["GIA CÔNG (Y/N)"]=""
-    # ncc["V/E U/CONG (Y/N)"]=""
-    # ncc["DÁN VNR (Y/N)"]=""
-    # ncc["K/L ĐB (Y/N)"]=""
-    ncc=ncc.head(50)
-    # A = ncc['TÊN NCC'].unique().tolist()
-    # B= ncc['MÃ'].unique().tolist()
+
+    sh2=gc.open('LSX - lưu trữ').worksheet('LSX ĐÃ IN')
+    sheet2=sh2.get_all_values()
+    lsx_cu=pd.DataFrame(sheet2)
+    lsx_cu.columns=lsx_cu.iloc[0]
+    list=lsx_cu['LỆNH SX'].unique().tolist()
+    # list
+    ncc=ncc[ncc["LỆNH SX"].isin(list)==False]
+    # ncc
     return ncc
 # ncc_list=ncc()
 
 def push_lsx(df,gc):
-    spreadsheet_key='1SHJmMVAo_9HgfTfzkDFIRw76-eLQPLDHZ3tOFj3ln5U'
+    spreadsheet_key='1JCyNuairaKmF0KL6Sj-7IegwrrGJ366TUnkUqNxBRAE'
     import gspread_dataframe as gd
     import gspread as gs
-    ws1 = gc.open("DSX2.1 - Lệnh sản xuất").worksheet("1. LENH SX")
+    ws1 = gc.open("Copy of DSX2.1 - Lệnh sản xuất").worksheet("1. LENH SX")
     existing1 = gd.get_as_dataframe(ws1)
-    updated1 = existing1.append(df)
+    # updated1 = existing1.append(df)
     gd.set_with_dataframe(ws1, df)
     # sheet_index_no1 = 0
     # sh = gc.open_by_key(spreadsheet_key)
     # worksheet1 = sh.get_worksheet(sheet_index_no1)
     # set_with_dataframe(worksheet1, df)
-    ws2 = gc.open("LSX - lưu trữ").worksheet("Sheet1")
+    ws2 = gc.open("LSX - lưu trữ").worksheet("LSX ĐÃ IN")
     existing2 = gd.get_as_dataframe(ws2)
     updated2 = existing2.append(df)
     gd.set_with_dataframe(ws2, updated2)
@@ -59,11 +58,11 @@ def push_lsx(df,gc):
     st.success('Done')
 
 
-def increment_counter(increment_value=0):
-    st.session_state.count += increment_value
+# def increment_counter(increment_value=0):
+#     rows += increment_value
 
-def decrement_counter(decrement_value=0):
-    st.session_state.count -= decrement_value
+# def decrement_counter(decrement_value=0):
+#     rows -= decrement_value
 credentials = service_account.Credentials.from_service_account_info(
 st.secrets["gcp_service_account"],
 scopes=['https://spreadsheets.google.com/feeds',
@@ -74,66 +73,70 @@ gc = gspread.authorize(credentials)
 
 st.cache()
 def form(df):
-    list_r=df["LỆNH SX"].unique().tolist()
-    c0,c1,c2,c3,c4,c5,c6=st.columns((1.8,1,.9,.9,.9,.9,.9))
+    with st.form(key='columns_in_form'):
+
+        list_r=df["LỆNH SX"].tolist()
+        # cols = st.beta_columns(5)
+        # for i, col in enumerate(cols):
+        c0,c1,c2,c3,c4,c5,c6= st.columns((1.8,1,.9,.9,.9,.9,.9))
+        rows = 10
 
 
 
-    # list_r=[50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200]
-    with c0:
-        lsx=[st.selectbox('LSX',list_r)]
-        for nr in range(st.session_state.count):
-            lsx.append(st.selectbox('',list_r[nr+1:], key=f'dfuestidn {nr}'))
-        # st.selectbox('Lệnh sản xuất',['a','b','c'])
-    with c1:
+        # list_r=[50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200]
+        with c0:
+            lsx=[]
+            for nr in range(rows):
+                lsx.append(c0.selectbox('',list_r[nr:], key=f'dfuestidn {nr}'))
+            # st.selectbox('Lệnh sản xuất',['a','b','c'])
+        with c1:
 
-        nm=[st.selectbox('Nhà máy',['NM1','NM3','NM5'])]
-        for nr in range(st.session_state.count):
-            nm.append(st.selectbox('',['NM1','NM3','NM5'], key=f'dfquestidn {nr}'))
-        # st.selectbox('Lệnh sản xuất',['a','b','c'])
-    with c2:
-        ldh=[st.radio('Loại đơn hàng',('C',"M"))]
-        for nr in range(st.session_state.count):
-            ldh.append(st.radio('Loại ĐH ',("C",'M'), key=f'dfquesatdidn {nr}'))  
-    with c3:
-        gc1=[st.radio('Gia công',("N","Y"))]
-        for nr in range(st.session_state.count):
-            gc1.append(st.radio('Gia công ',('N',"Y"), key=f'dfqudesưtdidn {nr}')) 
-    with c4:
-        uc=[st.radio('V/E uốn cong',("N","Y"))]
-        for nr in range(st.session_state.count):
-            uc.append(st.radio('V/e uốn cong ',('N',"Y"), key=f'dfqudesưtdidn{nr}')) 
-    with c5:
-        vn=[st.radio('Verneer',("N","Y"))]
-        for nr in range(st.session_state.count):
-            vn.append(st.radio('Verneer ',('N',"Y"), key=f'dfqudestưdidn {nr}')) 
-    with c6:
-        kl=[st.radio('Kim loại/HW',("N","Y"))]
-        for nr in range(st.session_state.count):
-            kl.append(st.radio('Kim loại ',('N',"Y"), key=f'dfqudestdidn1 {nr}')) 
+            nm=[]
+            for nr in range(rows):
+                nm.append(c1.selectbox('Nhà máy',["",'NM1','NM3','NM5'], key=f'dfquestidn {nr}'))
+            # st.selectbox('Lệnh sản xuất',['a','b','c'])
+        with c2:
+            ldh=[]
+            for nr in range(rows):
+                ldh.append(c2.selectbox('Loại đơn hàng',["",'C',"M"], key=f'dfquesatdidn {nr}'))  
+        with c3:
+            gc1=[]
+            for nr in range(rows):
+                gc1.append(c3.selectbox('Gia công ',["",'N',"Y"], key=f'dfqudesưtdidn {nr}')) 
+        with c4:
+            uc=[]
+            for nr in range(rows):
+                uc.append(c4.selectbox('V/e uốn cong ',["",'N',"Y"], key=f'dfqudesưtdidn{nr}')) 
+        with c5:
+            vn=[]
+            for nr in range(rows):
+                vn.append(c5.selectbox('Verneer ',["",'N',"Y"], key=f'dfqudestưdidn {nr}')) 
+        with c6:
+            kl=[]
+            for nr in range(rows):
+                kl.append(c6.selectbox('Kim loại ',["",'N',"Y"], key=f'dfqudestdidn1 {nr}')) 
+        st.form_submit_button('Submit')
     dict={"LỆNH SX":lsx,"NMSX":nm,"SẢN PHẨM (C/M)":ldh,"GIA CÔNG (Y/N)":gc1,"V/E U/CONG (Y/N)":uc,"DÁN VNR (Y/N)":vn,"K/L ĐB (Y/N)":kl}
     return dict
 if 'count' not in st.session_state:
-    st.session_state.count = 50
+    rows = 50
 t1,t2,t3,t4,t5,t6=st.columns((1,1,1,1,1,1))
-with t1:
-    st.button('Thêm dòng', on_click=increment_counter,
-        kwargs=dict(increment_value=5))
+# with t1:
+#     st.button('Thêm dòng', on_click=increment_counter,
+#         kwargs=dict(increment_value=1))
 
-    with t3:
-        st.write('Tổng số dòng = ', st.session_state.count+1)
 
 df=pull_lsx(gc)
 fi=st.selectbox('Chọn loại thao tác',['LSX mới','In lại LSX cũ'])
 if fi=='LSX mới':
 
     dict=form(df)
-
+    dff=pd.DataFrame.from_dict(dict)
+    # dff
+    lsx_info=dff.merge(df,how='left',on="LỆNH SX")
+    lsx_info
     if st.button('Push'):
-        dff=pd.DataFrame.from_dict(dict)
-        # dff
-        lsx_info=df.merge(dff,how='left',on="LỆNH SX")
-        lsx_info
+
         # Pull order_info
         # lsx_info=pull_lsx(gc)
         # lsx_info=lsx_info[["LỆNH SX",	"TÊN KHÁCH HÀNG",	"TÊN SẢN PHẨM TTF",	"SỐ LƯỢNG",	"ĐVT",	"LOẠI GỖ",	"MÀU SƠN"	,"NỆM"	,"NGÀY XUẤT",	"GHI CHÚ"]]
@@ -168,13 +171,13 @@ else:
 
 #     r1,r2,r3,r4,r5=st.columns((1,1,1,2,2))
 #     if 'count' not in st.session_state:
-#         st.session_state.count = 0
+#         rows = 0
 
 #     def increment_counter(increment_value=0):
-#         st.session_state.count += increment_value
+#         rows += increment_value
 
 #     def decrement_counter(decrement_value=0):
-#         st.session_state.count -= decrement_value
+#         rows -= decrement_value
     
 #     c1,c2,c3,c4,c5=st.columns((1,1,1,2,2))
 #     with c1:
@@ -184,23 +187,23 @@ else:
 #         st.button('Giảm dòng', on_click=decrement_counter,
 #             kwargs=dict(decrement_value=1))
 #     with c4:
-#         st.write('Tổng số dòng = ', st.session_state.count+1)
-#     h=st.session_state.count
+#         st.write('Tổng số dòng = ', rows+1)
+#     h=rows
 #     with r1:
 #         a=st.text_input('Dày',)
 
 
 #     with r2:
 #             b=[st.text_input('Rộng',)]
-#             for nr in range(st.session_state.count):
+#             for nr in range(rows):
 #                 b.append(st.text_input(label='', key=f'2`1 {nr}'))
 #     with r3:
 #             c=[st.text_input('Dài',)]
-#             for ng in range(st.session_state.count):
+#             for ng in range(rows):
 #                 c.append(st.text_input(label='', key=f'dfuestion {ng}'))
 #     with r4:
 #             d= [st.number_input('Số thanh',step=1)]
-#             for ngg in range(st.session_state.count):
+#             for ngg in range(rows):
 #                 d.append(st.number_input(label='', key=f'Quesdfgtion {ngg}',step=  1))
 
     
