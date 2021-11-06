1b8283a74003dff41785291edc07cd1a072fb29f
Hieu Thuan Lam <hieulam1312@gmail.com>
2021-11-06 10:58:32 +0700

    Update NVL_form_input.py

--- Prediction/NVL_form_input.py
+++ Prediction/NVL_form_input.py
@@ -21,7 +21,30 @@ import PIL
 # import cv
 st.set_page_config(layout='wide')
 
-from ncc import ncc_f
+def ncc_f():
+    import streamlit as st
+    import pandas as pd
+    from google.oauth2 import service_account
+    import gspread #-> Để update data lên Google Spreadsheet
+    from gspread_dataframe import set_with_dataframe #-> Để update data lên Google Spreadsheet
+    from oauth2client.service_account import ServiceAccountCredentials #-> Để nhập Google Spreadsheet Credentials
+    credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive'],
+    )
+    gc = gspread.authorize(credentials)
+    spreadsheet_key='1KBTVmlT5S2_x9VGseHdk_QDvZIfNBOLJy78lM0p3ORQ'
+
+    sh=gc.open('Kho NVL - NCC').worksheet('Sheet1')
+    sheet=sh.get_all_values()
+    ncc=pd.DataFrame(sheet)
+    ncc.columns=ncc.iloc[0]
+    ncc=ncc[1:]
+    # ncc
+    A = ncc['TÊN NCC'].unique().tolist()
+    B= ncc['MÃ'].unique().tolist()
+    return A,B
 abv=ncc_f()
 list_ncc=abv[0]
 list_int=abv[1]
