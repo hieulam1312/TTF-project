f6a001dd7d224c85367cbda0089feae699b2c58e
Hieu Thuan Lam <hieulam1312@gmail.com>
2021-11-13 15:28:44 +0700

    Update NVL_go_tron.py

--- Prediction/NVL_go_tron.py
+++ Prediction/NVL_go_tron.py
@@ -1,4 +1,3 @@
-# from typing_extensions import Concatenate
 import numpy as np
 from logging import error
 from mimetypes import MimeTypes
@@ -21,8 +20,33 @@ import PIL
 # import cv
 st.set_page_config(layout='wide')
 
-# from cvcv import ncc_f
-from ncc import abv
+def ncc_f():
+    import streamlit as st
+    import pandas as pd
+    from google.oauth2 import service_account
+    import gspread #-> Để update data lên Google Spreadsheet
+    from gspread_dataframe import set_with_dataframe #-> Để update data lên Google Spreadsheet
+    from oauth2client.service_account import ServiceAccountCredentials #-> Để nhập Google Spreadsheet Credentials
+    credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive'],
+    )
+    gc = gspread.authorize(credentials)
+    spreadsheet_key='1KBTVmlT5S2_x9VGseHdk_QDvZIfNBOLJy78lM0p3ORQ'
+
+    sh=gc.open('Kho NVL - NCC').worksheet('Sheet1')
+    sheet=sh.get_all_values()
+    ncc=pd.DataFrame(sheet)
+    ncc.columns=ncc.iloc[0]
+    ncc=ncc[1:]
+    # ncc
+    A = ncc['TÊN NCC'].unique().tolist()
+    B= ncc['MÃ'].unique().tolist()
+    return A,B
+abv=ncc_f()
+list_ncc=abv[0]
+list_int=abv[1]
 from list_info import qc_list
 go_list=["ALDER",
 "ASH VN",
@@ -64,35 +88,29 @@ go_list=["ALDER",
 ]
 in_list=["ADL","ASV","ASH","BDA","BEE","CXE","CSD","CSU","CHE","CCI","SYC","DUA","DLI","GON","HIC","KAP","LMU","MAP","MIT","MNG","NPL","OAK","PMU","PLR","REL","ROK","SOK","TAP","TEK","THO","TRM","TRU","WAL","WOK","WPR","WIL","XOA"]
 # abv
-list_ncc = abv[0]
 
-list_int= abv[1]
-# cv.ncc_f()
-# def increment_counter(increment_value=0):
-#     rowss += increment_value
-
-# def decrement_counter(decrement_value=0):
-#     rowss -= decrement_value
+import barcode
+from barcode.writer import ImageWriter
+def qr_code(link="https://engineering.catholic.edu/eecs/index.html"):
+        ean = barcode.get('code128', link, writer=ImageWriter())
+        filename = ean.save('code128',{"module_width":0.2, "module_height":6, "font_size":11, "text_distance": 1, "quiet_zone": 1})
+        return filename
 
 st.subheader('Nhập thông tin:')
-a2,a3,a4,a5=st.columns((1.5,1.5,1,1))
+
+# st.set_page_config(layout='wide')
+
+a2,a3,a4,a5,a6=st.columns((1.5,1.5,1,1,1))
 with a2:
     ncc=st.multiselect('NCC:',list_ncc)
 with a3:
     qc=st.multiselect('QC kiểm:',qc_list)
 with a4:
     go=st.multiselect('Loại gỗ:',go_list)
-with a5:
+with a6:
     da=st.text_input('Độ ẩm:',)
-cls1,cls2,cls3,cls4=st.columns(4)
-# with cls1:
-#     tk=st.number_input('Thẻ Kiện:',step=1)
-with cls2:
-    ml=st.text_input('MÃ LÔ:',)
-with cls3:
+with a5:
     clg=st.text_input('Chất lượng gỗ',)
-with cls4:
-    ngaykiem=st.text_input('Ngày kiểm',)
 if 'count' not in st.session_state:
     st.session_state.count = 0
 
@@ -114,110 +132,249 @@ with c4:
 h=st.session_state.count
 
 
-def form(ncc):
+if not ncc:
+    st.info('Nhập đầy đủ thông tin ở phía trên')
+else:
+    st.subheader('Danh sách kiểm chi tiết:')
+    # dv=st.selectbox('Đơn vị đo:',['mm','Inch','feet'])
     with st.form(key='columns_in_form'):
-        rowss=60
-        if not ncc:
-            st.info('Nhập đầy đủ thông tin ở phía trên')
-        else:
-            st.subheader('Danh sách kiểm chi tiết:')
-            mol1,mol2=st.columns(2)
-            with mol1:
-                mol1.subheader('KIỆN 1')
+        r1,r2,r3,r4,r5=st.columns((1,1,1,2,2))
+
+        with r1:
+            a=r1.text_input('Dày',)
 
 
-            r_1,r_2,r_3,t1_,r_4=st.columns((1,1,1,1,3))
-            r1,r2,r3,r4=st.columns((1,1,2,3))
-              
-            with r_1:
-                a1=r_1.text_input('Dày',)
-            with r_2:
-                tk1=r_2.text_input('Thẻ kiện',)
-            with r_3:
-                st.form_submit_button('Kiểm tra số khối')
+        with r2:
+                b=[]
+                for nr in range(st.session_state.count):
+                    b.append(r2.text_input(label='Rộng', key=f'2`1 {nr}'))
+        with r3:
+                c=[]
+                for ng in range(st.session_state.count):
+                    c.append(r3.text_input(label='Dài', key=f'dfuestion {ng}'))
+        with r4:
+                d= []
+                for ngg in range(st.session_state.count):
+                    d.append(r4.number_input(label='Số thanh', key=f'Quesdfgtion {ngg}',step=  1))
 
-            with r1:
-                b1=[]
-                for nr in range(rowss):
-                    b1.append(r1.text_input('Rộng', key=f'df1uestidn {nr}'))
-            with r2:
-                    c1=[]
-                    for ng in range(rowss):
-                        c1.append(r2.text_input(label='Dài', key=f'df1uestion {ng}'))
-            with r3:
-                    d1= []
-                    for ngg in range(rowss):
-                        d1.append(r3.text_input(label='Số thanh', key=f'Quđsesdf1gtion {ngg}'))
-            
-            tk1="-" if tk1 =="" else tk1
-            a1="0" if a1 =="" else a1
-            b1=["0" if v =="" else v for v in b1]
-            c1=["0" if v =="" else v for v in c1]
-            d1=["0" if v =="" else v for v in d1]
-            dict1={'MÃ THẺ KIỆN':tk1,'QC Dày':a1,'QC Rộng':b1,'QC Dài':c1,'Số thanh':d1}
-            df1=pd.DataFrame.from_dict(dict1)    
-            df1=df1.astype({'QC Rộng':float,'QC Dài':float,'QC Dày':float,'Số thanh':int,'MÃ THẺ KIỆN':str})
-            khoi=df1['QC Dày']*df1['QC Rộng']*df1['QC Dài']*df1['Số thanh']
-            df1['MÃ THẺ KIỆN']="K."+in_list[go_list.index(go[0])]+"."+df1['MÃ THẺ KIỆN'].astype(str)
-            df1=df1[df1['Số thanh']>0]
-            df1['KHỐI LƯỢNG']=round(khoi/10**9,4)
-            td=pd.to_datetime('today')
-            df1['NGÀY NHẬP LIỆU']=td
-            # df1['THẺ KIỆN']=tk
-            df1['NCC']=ncc[0]
-            df1['LOẠI GỖ']=go[0]
-            df1['NGƯỜI KIỂM']=qc[0]
-            df1['NGÀY NHẬP LIỆU']=df1['NGÀY NHẬP LIỆU'].dt.date 
-            NCC=ncc[0]+" "+"("+clg+")"
-            df1['NCC']=  NCC
-            df1['MÃ LÔ']=ml
-            # df['NCC']=NCC
-            df1['ĐỘ ẨM']=da
-            df1["NGÀY KIỂM"]=ngaykiem
-            total=round(sum(df1['KHỐI LƯỢNG']),4)
-            d1=df1.sort_index(ascending=False).reset_index(drop=True) 
-            with r4:
-                df2=df1.groupby(['MÃ THẺ KIỆN','QC Dài']).agg({'KHỐI LƯỢNG':'sum'}).reset_index()
-                df2
-            return df1
-data=form(ncc)
-# data
-ncc_index=list_ncc.index(ncc[0])
-ini=list_int[ncc_index]
-def eccount(df,ini):
+    
+        b=["0" if v =="" else v for v in b]
+        c=["0" if v =="" else v for v in c]
+        d=["0 "if v =="" else v for v in d]
+
+        # a
+        b1=[]
+        c1=[]
+        # a1=a.replace(',','.')
+        st.form_submit_button('Submit')
+
+        for b_ in b:
+            new_string = b_.replace(',','.')
+            b1.append(new_string)
+        for c_ in c:
+            new_string = c_.replace(',','.')
+            c1.append(new_string)
+
+
+        ncc_index=list_ncc.index(ncc[0])
+        ini=list_int[ncc_index]
+
+        dict={'Rộng':b1,'Dài':c1,'Số thanh':d}
+    
+        import pandas as pd
+        df=pd.DataFrame.from_dict(dict)    
+        df=df.astype(float)
+        df['Dày']= int(a)
+
+        khoi=df['Dày']*df['Rộng']*df['Dài']*df['Số thanh']
+
+        df['SỐ KHỐI']=round(khoi/10**9,4)
+        td=pd.to_datetime('today')
+        df['NGÀY KIỂM']=td
+        # df['THẺ KIỆN']=tk
+        df['NCC']=ncc[0]
+        df['LOẠI GỖ']=go[0]
+        df['QC KIỂM']=qc[0]
+        df['NGÀY KIỂM']=df['NGÀY KIỂM'].dt.date 
+
+        total=round(sum(df['SỐ KHỐI']),4)
+        df=df[df['SỐ KHỐI']>0]
+        d1=df.sort_index(ascending=False).reset_index(drop=True)      
+        #Cân đối số liệu
+        # _du=0
+    st.subheader('Cân đối số liệu')
+    cl1,cl2=st.columns(2)
+    with cl1:
+        ncc_num=st.number_input('Số khối NCC:',format="%.4f")
+    with cl2: 
+        st.write('**Tổng số khối thực kiểm:** ',total)
+        
+        _du=total-ncc_num
+
+        if _du>0.001:
+
+            _row0=d1.head(1)
+            stt=_row0['Số thanh'].tolist()
+            sk=(_row0['Dày']*_row0['Rộng']*_row0['Dài']*_row0['Số thanh'])/10**9
+
+            test=((_du)*(10**9))/(_row0['Dày']*_row0['Rộng']*_row0['Dài'])
+            # _row0['Số thanh']
+            # 
+            st.write('**Điều chỉnh số thanh mã cuối cùng thành:**',stt[0]-round(test[0],0))
+
+
+    cls1,cls2,cls3=st.columns(3)
+    with cls1:
+        tk1=st.number_input('Thẻ Kiện:',step=1)
+    with cls2:
+        ml=st.text_input('Mã lô:',)
+        
+
+    st.subheader('KẾT QUẢ:')
+
+    c1,c2=st.columns(2)
+    with c1:
+        tk="K."+in_list[go_list.index(go[0])]+"."+str(tk1)
+        st.write('**Thẻ kiện:** ',tk)
+        st.write('**Mã lô:** ',ml)
+        NCC=ncc[0]+" "+"("+clg+")"
+        st.write('**NCC:** ',NCC)
+    df['THẺ KIỆN']=tk
+    df['Mã lô']=ml
+    # df['NCC']=NCC
+    df['ĐỘ ẨM']=da
+    # with c2:
+    #    image= image=st.image(qr_code(link=tk))
+
+    df2=df[['Dày','Rộng','Dài','Số thanh','SỐ KHỐI']]
+    df2
+    df2['Số thanh']=df2['Số thanh'].astype(int)
+    df2['SỐ KHỐI']=df2['SỐ KHỐI'].astype(str)
+
+    df2=df2.astype(str)
+    df2=df2.replace("0"," ")
+    df2=df2.replace("0.0"," ")
+    st.write('**Tổng số khối:** ',total)
+
+len_=len(df.index.tolist())
+
+if len_ >20:
+    df_20=df2.iloc[:19]
+    df_20
+    df_o=df_20.copy()
+    df_o=df_o.notnull()
+    df_o=df_o.replace(True,0)
+    # df_o
+    df_ov=df2.iloc[20:].reset_index(drop=True)
+    df_o.loc[df_ov.index, :] = df_ov[:]
+    df_o=df_o.astype(str)
+    df_o=df_o.replace("0","-")
+    df_over=df_o.copy()
+    # df_over
+    # df_over=df_0.loc[df1.index, :] = df1[:]
+    html = """ DANH SÁCH THẺ KIỆN\n
+            <body> 
+            <table  margin-bottom= "2000" cellpadding="2" cellspacing="2" padding="10">     
+            <tr>         
+            <td>             
+            <table  margin-bottom= "2000" cellpadding="2" cellspacing="2" padding="10">                  
+            <tr>                     
+            <td>{0}<td> 
+            <td>{1}</td>                 
+            </tr>             
+            </table>         
+            </td>     
+            </tr> 
+            </table> 
+            </body>
+                """.format(df_20.to_html(index=False,col_space=50),df_over.to_html(index=False,col_space=50))
+else:
+    html = """ DANH SÁCH THẺ KIỆN\n
+            <html>
+            <br>
+            <head></head>
+            <body>
+                {0}
+                <br>
+            </body>
+            </html>
+            """.format(df2.to_html(index=False,col_space=100,justify='center'))
+def send_email(subject,total,tk,QC,NCC,qc,ml,td,html,receiver_list):
+    # (1) Create the email head (sender, receiver, and subject)
+    sender_email = st.secrets['SENDER_EMAIL']
+    password = st.secrets['PWD_EMAIL']
+    # receiver_email='hieulam1312@gmail.com'
+    email = MIMEMultipart()
+    email["From"] = sender_email
+    # email["To"] = 'abc'
+    email['Subject']=subject
+
+
+
+    html3="""
+    <html>
+    Tổng số khối: <b>{}</b><br>
+    QC kiểm: <b>{}</b><br>
+    Mã lô: <b>{}</b><br>
+    Ngày kiểm: <b>{}</b><br>
+    </html>
+    """.format(total,qc,ml,td)
+
+
+   
+    fp = open('code128.png', 'rb')
+    msgImage = MIMEImage(fp.read())
+    fp.close()
+    msgImage.add_header('Content-ID', 'barcode')
+    email.attach(msgImage)
+
+
+    part1 = MIMEText(html, 'html')
+    part2=MIMEText(html3,'html')
+    # part3=MIMEText(html4,'html')
+    email.attach(part2)
+    # email.attach(part3)
+    email.attach(part1)
+    try:
+        session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+        session.starttls() #enable security
+        session.login(sender_email, password) #login with mail_id and password
+    
+        for receiver_email in receiver_list:
+            email['To'] = receiver_email
+            text = email.as_string()
+            session.sendmail(sender_email, receiver_email, text)
+        st.success('Đã gửi mail thành công')
+        return(1)
+    except:
+        print("Could not send mail to {}".format(receiver_email))
+        return(0)
+
+
+# new_dict = df.groupby('THẺ KIỆN').apply(lambda x: x.values.tolist()).to_dict()
+
+def eccount():
     df4=df.copy()
-#     df4
-    uni_tk=df4["MÃ THẺ KIỆN"].unique().tolist()
-    uni_dai=df4['QC Dài'].unique().tolist()
-#     uni_dai
-
-    # uni_dai=uni_dai.sort()
-#     uni_dai
+    uni_tk=df4['THẺ KIỆN'].unique().tolist()
+    uni_dai=df4['Dài'].unique().tolist()
+    uni_dai.sort()
     if len(uni_dai)==2:
+
         string_dai=str(int(uni_dai[0]))+"/"+str(int(uni_dai[-1]))
     elif len(uni_dai)==1:
         string_dai=str(int(uni_dai[0]))
     else:
+
         string_dai=str(int(uni_dai[0]))+"-"+str(int(uni_dai[-1]))
-#     string_dai
-    df4['QC Dài 2']=string_dai
-    df4["MÃ THẺ KIỆN2"]=df4["MÃ THẺ KIỆN"]
-    df4["MÃ THẺ KIỆN3"]=df4["MÃ THẺ KIỆN"]
-    df4['QC Dày2']=df['QC Dày']
+    df4['DÀI 2']=string_dai
+    df4['THẺ KIỆN2']=tk
+    df4['THẺ KIỆN 3']=tk
+    df4['Dày2']=df['Dày']
     df4["ncc"]=ini
     df4['Loại Gỗ']=in_list[go_list.index(go[0])]
+    eccount=df4[['THẺ KIỆN','THẺ KIỆN2','THẺ KIỆN 3','Dày','DÀI 2','Mã lô','Loại Gỗ','Dày2','ncc','SỐ KHỐI']]
 
-    eccount=df4[["MÃ THẺ KIỆN","MÃ THẺ KIỆN2","MÃ THẺ KIỆN3",'QC Dày','QC Dài 2','MÃ LÔ','Loại Gỗ','QC Dày2','ncc','KHỐI LƯỢNG']]
-
-    eccount_gr=eccount.groupby(["MÃ THẺ KIỆN","MÃ THẺ KIỆN2","MÃ THẺ KIỆN3",'QC Dày','QC Dài 2','MÃ LÔ','Loại Gỗ','QC Dày2','ncc'])['KHỐI LƯỢNG'].sum().reset_index()
-    eccount_gr['Tỉ lệ']=1
-    eccount_gr['Đơn vị']="m3"
-    eccount_gr['Giá mua']=round(eccount_gr['Tỉ lệ']/eccount_gr["KHỐI LƯỢNG"],6)
-    eccount_gr['Giá mua2']=0
-    eccount_gr['Giá bán']=eccount_gr['Giá mua']
-    eccount_gr['Giá bán2']=0
-    eccount_gr['Ecount']="Ecount"
-#     eccount_gr
+    eccount_gr=eccount.groupby(['THẺ KIỆN','THẺ KIỆN2','THẺ KIỆN 3','Dày','DÀI 2','Mã lô','Loại Gỗ','Dày2','ncc'])['SỐ KHỐI'].sum().reset_index()
     return eccount_gr
 
 def push(df,str):
@@ -233,14 +390,14 @@ def push(df,str):
          'https://www.googleapis.com/auth/drive'],
     )
     gc = gspread.authorize(credentials)
-    spreadsheet_key='1_ZhSbjL2EfbyTLyWCpTrHJi6kCku1j0eVwQ_g1R2QTM'
+    spreadsheet_key='1KBTVmlT5S2_x9VGseHdk_QDvZIfNBOLJy78lM0p3ORQ'
 
     import gspread_dataframe as gd
     import gspread as gs
 
-    ws = gc.open("TTF - Nhập liệu gỗ tròn").worksheet(str)
+    ws = gc.open("Kho NVL - NCC").worksheet(str)
     existing = gd.get_as_dataframe(ws)
-    # existing
+
     updated = existing.append(df)
     gd.set_with_dataframe(ws, updated)
     st.success('Tải lại trang để tiếp tục nhập liệu')
@@ -248,17 +405,10 @@ def push(df,str):
 
 
 list_email=['qlcl@tanthanhgroup.com','ttf.qcgo@gmail.com']
-with c5:
-    if st.button('Xuất danh sách'):
-        # send_email("Thẻ kiện: "+tk+" - "+NCC+" - "+qc[0],total,tk,qr_code(link=tk),NCC,qc[0],ml,td,html,list_email)
-        sheet='3. DS NHẬP ECOUNT'
-        # from cv import push
-        ECC=eccount(data,ini)
-        push(ECC,sheet)
-        data['MÃ THẺ KIỆN_2']=data['MÃ THẺ KIỆN'].str.replace('K','T')
-        # data
-        data=data[['MÃ THẺ KIỆN_2',"MÃ THẺ KIỆN","NGÀY NHẬP LIỆU","NGÀY KIỂM",	"NGƯỜI KIỂM",	"NCC",	"LOẠI GỖ",	"QC Dày",	"QC Rộng","QC Dài",	"Số thanh",	 "KHỐI LƯỢNG", 	"MÃ LÔ",'ĐỘ ẨM']]
-        data=data[data["QC Dài"]>0]
-        data
-        push(data,'1. NHẬP LIỆU')
-
+if st.button('Hoàn tất'):
+    send_email("Thẻ kiện: "+tk+" - "+NCC+" - "+qc[0],total,tk,qr_code(link=tk),NCC,qc[0],ml,td,html,list_email)
+    sheet='Ecount'
+    # from cv import push
+    ECC=eccount()
+    push(ECC,sheet)
+    push(df,'Sheet2')
