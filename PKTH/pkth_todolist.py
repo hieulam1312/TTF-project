df6acb16923d6aa3d80d9618f8f9588c90c6b8ca
PKTHttf <91238030+PKTHttf@users.noreply.github.com>
2021-11-19 10:51:33 +0700

    Update pkth_todolist.py

--- PKTH/pkth_todolist.py
+++ PKTH/pkth_todolist.py
@@ -1,4 +1,6 @@
 from abc import ABCMeta
+# from typing_extensions import Concatenate
+from pandas.core.reshape.concat import concat
 from pyasn1_modules.rfc2459 import Name
 import streamlit as st
 import gspread_dataframe
@@ -15,58 +17,6 @@ from gspread_dataframe import set_with_dataframe
 
 st.set_page_config(layout='wide')
 
-
-
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive'],
-)
-gc1 = gspread.authorize(credentials)
-spreadsheet_key = '1XwE7OoVitWw0kIo0N2Ykxh5luZ_xHizLAECwLQJiLfE'
-sh1=gc1.open("test").worksheet('Sheet3')
-
-# df_done=pd.DataFrame(df_)
-# if df_done.empty:
-sample_name=sh1.get_all_values()
-df=pd.DataFrame(sample_name)
-df.columns=df.iloc[0]
-df=df[1:]
-
-def data(df):
-    df['CÔNG VIỆC']=df['TÊN SẢN PHẨM']+" - "+df['LOẠI CÔNG VIỆC']
-    staff_list=['Đệ','Thuận','Trọn','Linh','Vân','Duy']
-    todo=df[['CÔNG VIỆC','LOẠI CÔNG VIỆC']]
-    SXM=todo[todo['LOẠI CÔNG VIỆC']=='SX MỚI']
-    DHM=todo[todo['LOẠI CÔNG VIỆC'].str.contains('MẪU')]
-    CNC=todo[todo['LOẠI CÔNG VIỆC'].str.contains('CNC')]
-    BG=todo[todo['LOẠI CÔNG VIỆC'].str.contains('PHIẾU YC')]
-    todo_list=df['CÔNG VIỆC'].unique().tolist()
-
-    c1,c2,c3=st.columns((1,3,2))
-    with c1:
-        name=st.selectbox('Tên của bạn:',staff_list)
-    c2,c3,c4,c5,c6=st.columns(5)
-    plan_done=[]
-    with c2:
-        plan_done1=st.multiselect('Sản xuất mới',SXM)
-
-    with c3:
-        plan_done2=st.multiselect('ĐH mẫu',DHM)
-    with c4:
-        plan_done3=st.multiselect('CNC',CNC)
-    with c5:
-        plan_done4=st.multiselect('Báo giá',BG)
-    with c6:
-        out_plan=st.text_area('CÔNG VIỆC khác:',)
-    plan_done=plan_done1+plan_done2+plan_done3+plan_done4
-    pl=pd.DataFrame(plan_done,columns=['CÔNG VIỆC'])
-    pl['NV']=name
-    t=out_plan.split('\n')
-    done=pd.DataFrame(t,columns=['CÔNG VIỆC'])
-    done['NV']=name
-    return done,pl,todo
-# @st.cache()
 def run(done,pl,todo):
     import gspread_dataframe as gd
     import gspread as gs
@@ -79,6 +29,8 @@ def run(done,pl,todo):
     up_done=up_done.replace(np.nan,"Công việc khác")
     up_done=up_done[up_done['CÔNG VIỆC'].isnull()==False]
     updated = done_day1.append(up_done)
+    updated=updated.replace("",np.nan)
+    updated=updated.dropna()
     gd.set_with_dataframe(sh2, updated)
     done_dayY = gd.get_as_dataframe(sh2)
     done_dayY['NGÀY']=done_dayY['NGÀY'].astype('datetime64')
@@ -116,15 +68,65 @@ def run(done,pl,todo):
         Duy=done_day[done_day['NV']=='Duy'].reset_index(drop=True)
         Duy[['LOẠI CÔNG VIỆC','CÔNG VIỆC']]
 
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive'],
+)
+gc1 = gspread.authorize(credentials)
+spreadsheet_key = '1XwE7OoVitWw0kIo0N2Ykxh5luZ_xHizLAECwLQJiLfE'
+sh1=gc1.open("test").worksheet('Sheet3')
+
+# df_done=pd.DataFrame(df_)
+# if df_done.empty:
+sample_name=sh1.get_all_values()
+df=pd.DataFrame(sample_name)
+df.columns=df.iloc[0]
+df=df[1:]
 st.title(':star:CÁC VIỆC ĐÃ HOÀN THÀNH HÔM NAY:star:')
-d=data(df)
-done=d[0]
-pl=d[1]
-todo=d[2]
-if st.button('Xác nhận'):
+
+with st.form(key='abc'):
+    df['CÔNG VIỆC']=df['TÊN SẢN PHẨM']+" - "+df['LOẠI CÔNG VIỆC']
+    staff_list=['Đệ','Thuận','Trọn','Linh','Vân','Duy']
+    todo=df[['CÔNG VIỆC','LOẠI CÔNG VIỆC']]
+    SXM=todo[todo['LOẠI CÔNG VIỆC']=='SX MỚI']
+    DHM=todo[todo['LOẠI CÔNG VIỆC'].str.contains('MẪU')]
+    CNC=todo[todo['LOẠI CÔNG VIỆC'].str.contains('CNC')]
+    BG=todo[todo['LOẠI CÔNG VIỆC'].str.contains('PHIẾU YC')]
+    todo_list=df['CÔNG VIỆC'].unique().tolist()
+
+    c1,c2,c3=st.columns((1,3,2))
+    with c1:
+        name=st.selectbox('Tên của bạn:',staff_list)
+    c2,c3,c4,c5,c6=st.columns(5)
+    plan_done=[]
+    with c2:
+        plan_done1=st.multiselect('Sản xuất mới',SXM)
+
+    with c3:
+        plan_done2=st.multiselect('ĐH mẫu',DHM)
+    with c4:
+        plan_done3=st.text_area('CNC',)
+    with c5:
+        plan_done4=st.multiselect('Báo giá',BG)
+    with c6:
+        out_plan=st.text_area('CÔNG VIỆC khác:',)
+    plan_done=plan_done1+plan_done2+plan_done4
+    pl=pd.DataFrame(plan_done,columns=['CÔNG VIỆC'])
+    cnc=plan_done3.split('\n')
+    cnc_done=pd.DataFrame(cnc,columns=['CÔNG VIỆC'])
+    cnc_done['NV']=name
+    pl['NV']=name
+    t=out_plan.split('\n')
+    done=pd.DataFrame(t,columns=['CÔNG VIỆC'])
+    done['NV']=name
+    pl=pl.astype(str)
+    done_=pd.concat([done,cnc_done]).reset_index(drop=True)
+    done_=done_.astype(str)
+    st.form_submit_button("Xác nhận")
+
+
+if st.button('Kết quả tuần này'):
 
     # st.title('THÀNH QUẢ CỦA HÔM NÀY NÈ!:smile:')
     run(done,pl,todo)
-
-# if st.button('Thành quả tuần này!'):
-#     st.success()
