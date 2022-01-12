cb0fbe3c47fbe22cb08e919f5857532345d33b07
PKTHttf <91238030+PKTHttf@users.noreply.github.com>
2022-01-12 10:46:12 +0700

    Update khoson.py

--- TTT_ver2/khoson.py
+++ TTT_ver2/khoson.py
@@ -173,7 +173,7 @@ elif thaotac=='Xuất kho':
         the_table = ax.table(cellText = data.values, colLabels = data.columns,loc='bottom')
 #         the_table.auto_set_font_size(False)
         the_table.set_fontsize(20)
-        the_table.scale(2, 2)
+        the_table.scale(1,1)
         pp = PdfPages("phieu_xuat_kho.pdf")
         pp.savefig(fig, bbox_inches = 'tight')
         pp.close()
