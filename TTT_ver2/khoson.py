2d894da944cf728aedbb067425cc1086512b89f8
PKTHttf <91238030+PKTHttf@users.noreply.github.com>
2022-01-12 13:46:38 +0700

    Update khoson.py

--- TTT_ver2/khoson.py
+++ TTT_ver2/khoson.py
@@ -167,8 +167,8 @@ elif thaotac=='Xuất kho':
         push(data1,gc,'Xuất kho')
         data2=data1.drop(columns={'Nhà máy','Lệnh SX','Ngày xuất kho','Công đoạn'})
         fig, ax = plt.subplots(figsize = (4,.2))
-        ax.set_title('TTF - Phiếu xuất kho ngày {}'.format(pd.to_datetime('today').date()),size=12,loc='left')
-        plt.suptitle('LSX: {} - Nhà máy: {} - Công đoạn: {}'.format(lsx[0],nm[0],cd[0]),size=8,ha='right')
+        ax.set_title('TTF - Phiếu xuất kho ngày {}'.format(pd.to_datetime('today').date()),size=10,loc='left')
+        plt.suptitle('LSX: {} - Nhà máy: {} - Công đoạn: {}'.format(lsx[0],nm[0],cd[0]),size=6,ha='left')
         ax.axis('tight')
         ax.axis('off')
 
