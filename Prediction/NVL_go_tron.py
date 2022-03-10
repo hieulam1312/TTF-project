e912312f9cf41b66d5aa6b78be01b8c5099a3f89
PKTHttf <91238030+PKTHttf@users.noreply.github.com>
2022-03-10 08:35:26 +0700

    Update NVL_go_tron.py

--- Prediction/NVL_go_tron.py
+++ Prediction/NVL_go_tron.py
@@ -102,102 +102,126 @@ def increment_counter(increment_value=0):
 def decrement_counter(decrement_value=0):
     st.session_state.count -= decrement_value
 
-c1,c2,c3,c4,c5=st.columns((1,1,1,2,2))
+c1,c2,c3,c4,c5=st.columns((1,1,1,1,1))
 with c1:
     st.button('Thêm dòng', on_click=increment_counter,
         kwargs=dict(increment_value=1))
 with c2:
     st.button('Giảm dòng', on_click=decrement_counter,
         kwargs=dict(decrement_value=1))
-with c4:
+with c3:
     st.write('Tổng số dòng = ', st.session_state.count+1)
 h=st.session_state.count
 
 
 def form(ncc):
-    with st.form(key='columns_in_form'):
-        rowss=60
-        if not ncc:
-            st.info('Nhập đầy đủ thông tin ở phía trên')
-        else:
-            st.subheader('Danh sách kiểm chi tiết:')
-            mol1,mol2=st.columns(2)
-            with mol1:
-                mol1.subheader('KIỆN 1')
+    
+    r_1,r_2,r_3,t1_,r_4=st.columns((1,1,1,1,3))
+    
+    with r_1:
+        a1=r_1.text_input('Dày',)
+    with r_2:
+        tk1=r_2.text_input('Thẻ kiện',)
 
+    placeholder = st.empty()
 
-            r_1,r_2,r_3,t1_,r_4=st.columns((1,1,1,1,3))
-            r1,r2,r3,r4=st.columns((1,1,2,3))
-              
-            with r_1:
-                a1=r_1.text_input('Dày',)
-            with r_2:
-                tk1=r_2.text_input('Thẻ kiện',)
-            with r_3:
-                st.form_submit_button('Kiểm tra số khối')
+    with placeholder.container():
+    # dv=st.selectbox('Đơn vị đo:',['mm','Inch','feet'])
+        with st.form(key='columns_in_form'):
+            rowss=60
+            if not ncc:
+                st.info('Nhập đầy đủ thông tin ở phía trên')
+            else:
+                st.subheader('Danh sách kiểm chi tiết:')
+                mol1,mol2=st.columns(2)
+                with mol1:
+                    mol1.subheader('KIỆN 1')
+                with mol2:
+                    st.form_submit_button('Kiểm tra số khối')
+                r1,r2,r3,r4,r5=st.columns((1,1,1,2,2))
+                with r1:
+                    # placeholder = r2.empty()
+                    with placeholder.container():
+                        b1=[]
+                        for nr in range(5+st.session_state.count):
+                            # b=r2.text_input('Dài',)
+                    
+                            b1.append(r1.text_input(label='Rộng', key=f'2`1 {nr}'))
+                with r2:
+                    # placeholder2 = r3.empty()
+                    with placeholder.container():
+                        c1=[]
+                        for nr in range(5+st.session_state.count):
+                                    # b=r2.text_input('Dài',)
+                            
+                            c1.append(r2.text_input(label='Dài', key=f'2`1 {nr}'))
+                    # c.append(placeholder.text_input(label='Dài', key=f'dfuestion {ng}'))
+                with r3:            
+                    # placeholder2 = r4.empty()
+                    with placeholder.container():
+                        d1=[]
+                        for nr in range(5+st.session_state.count):
+                            
+                            d1.append(r3.text_input(label='Số thanh', key=f'Quesdfgtion {nr}'))
 
-            with r1:
-                b1=[]
-                for nr in range(rowss):
-                    b1.append(r1.text_input('Rộng', key=f'df1uestidn {nr}'))
-            with r2:
-                    c1=[]
-                    for ng in range(rowss):
-                        c1.append(r2.text_input(label='Dài', key=f'df1uestion {ng}'))
-            with r3:
-                    d1= []
-                    for ngg in range(rowss):
-                        d1.append(r3.text_input(label='Số thanh', key=f'Quđsesdf1gtion {ngg}'))
-            
-            tk1="-" if tk1 =="" else tk1
-            a1="0" if a1 =="" else a1
-            b1=["0" if v =="" else v for v in b1]
-            c1=["0" if v =="" else v for v in c1]
-            d1=["0" if v =="" else v for v in d1]
-            dict1={'MÃ THẺ KIỆN':tk1,'QC Dày':a1,'QC Rộng':b1,'QC Dài':c1,'Số thanh':d1}
-            df1=pd.DataFrame.from_dict(dict1)    
-            df1=df1.astype({'QC Rộng':float,'QC Dài':float,'QC Dày':float,'Số thanh':int,'MÃ THẺ KIỆN':str})
-            khoi=df1['QC Dày']*df1['QC Rộng']*df1['QC Dài']*df1['Số thanh']
-            df1['MÃ THẺ KIỆN']="K."+in_list[go_list.index(go[0])]+"."+df1['MÃ THẺ KIỆN'].astype(str)
-            df1=df1[df1['Số thanh']>0]
-            df1['KHỐI LƯỢNG']=round(khoi/10**9,4)
-            td=pd.to_datetime('today')
-            df1['NGÀY NHẬP LIỆU']=td
-            # df1['THẺ KIỆN']=tk
-            df1['NCC']=ncc[0]
-            df1['LOẠI GỖ']=go[0]
-            df1['NGƯỜI KIỂM']=qc[0]
-            df1['NGÀY NHẬP LIỆU']=df1['NGÀY NHẬP LIỆU'].dt.date 
-            NCC=ncc[0]+" "+"("+clg+")"
-            df1['NCC']=  NCC
-            df1['MÃ LÔ']=ml
-            # df['NCC']=NCC
-            df1['ĐỘ ẨM']=da
-            df1["NGÀY KIỂM"]=ngaykiem
-            total=round(sum(df1['KHỐI LƯỢNG']),4)
-            d1=df1.sort_index(ascending=False).reset_index(drop=True) 
-            with r4:
-                df2=df1.groupby(['MÃ THẺ KIỆN','QC Dài']).agg({'KHỐI LƯỢNG':'sum'}).reset_index()
-                df2
-            return df1
+        with c4:
+            if st.button("Xóa nội dung thẻ kiện cũ"):
+                placeholder.empty()        
+       
+
+        tk1="-" if tk1 =="" else tk1
+        a1="0" if a1 =="" else a1
+        b1=["0" if v =="" else v for v in b1]
+        c1=["0" if v =="" else v for v in c1]
+        d1=["0" if v =="" else v for v in d1]
+        dict1={'MÃ THẺ KIỆN':tk1,'QC Dày':a1,'QC Rộng':b1,'QC Dài':c1,'Số thanh':d1}
+        df1=pd.DataFrame.from_dict(dict1)    
+        df1=df1.astype({'QC Rộng':float,'QC Dài':float,'QC Dày':float,'Số thanh':int,'MÃ THẺ KIỆN':str})
+        df1['Số thanh']=df1['Số thanh'].astype(int)
+        khoi=df1['QC Dày']*df1['QC Rộng']*df1['QC Dài']*df1['Số thanh']
+        df1['MÃ THẺ KIỆN']="K."+in_list[go_list.index(go[0])]+"."+df1['MÃ THẺ KIỆN'].astype(str)
+        df1=df1[df1['Số thanh']>0]
+        df1['KHỐI LƯỢNG']=round(khoi/10**9,4)
+        td=pd.to_datetime('today')
+        df1['NGÀY NHẬP LIỆU']=td
+        # df1['THẺ KIỆN']=tk
+        df1['NCC']=ncc[0]
+        df1['LOẠI GỖ']=go[0]
+        df1['NGƯỜI KIỂM']=qc[0]
+        df1['NGÀY NHẬP LIỆU']=df1['NGÀY NHẬP LIỆU'].dt.date 
+        NCC=ncc[0]+" "+"("+clg+")"
+        df1['NCC']=  NCC
+        df1['MÃ LÔ']=ml
+        # df['NCC']=NCC
+        df1['ĐỘ ẨM']=da
+        df1["NGÀY KIỂM"]=ngaykiem
+        
+        total=round(sum(df1['KHỐI LƯỢNG']),4)
+        d1=df1.sort_index(ascending=False).reset_index(drop=True) 
+        with r4:
+            df2=df1.groupby(['MÃ THẺ KIỆN','QC Dài']).agg({'KHỐI LƯỢNG':'sum'}).reset_index()
+            df2
+        return df1
 data=form(ncc)
 # data
 ncc_index=list_ncc.index(ncc[0])
 ini=list_int[ncc_index]
 def eccount(df,ini):
     df4=df.copy()
-    # df4
+
     uni_tk=df4["MÃ THẺ KIỆN"].unique().tolist()
     uni_dai=df4['QC Dài'].unique().tolist()
-    uni_dai.sort()
-    if len(uni_dai)==2:
 
+
+    # uni_dai=uni_dai.sort()
+
+    if len(uni_dai)==2:
         string_dai=str(int(uni_dai[0]))+"/"+str(int(uni_dai[-1]))
     elif len(uni_dai)==1:
         string_dai=str(int(uni_dai[0]))
     else:
-
         string_dai=str(int(uni_dai[0]))+"-"+str(int(uni_dai[-1]))
+
     df4['QC Dài 2']=string_dai
     df4["MÃ THẺ KIỆN2"]=df4["MÃ THẺ KIỆN"]
     df4["MÃ THẺ KIỆN3"]=df4["MÃ THẺ KIỆN"]
@@ -211,11 +235,11 @@ def eccount(df,ini):
     eccount_gr['Tỉ lệ']=1
     eccount_gr['Đơn vị']="m3"
     eccount_gr['Giá mua']=round(eccount_gr['Tỉ lệ']/eccount_gr["KHỐI LƯỢNG"],6)
-    eccount_gr['Giá mua2']=""
+    eccount_gr['Giá mua2']=0
     eccount_gr['Giá bán']=eccount_gr['Giá mua']
-    eccount_gr['Giá bán2']=""
+    eccount_gr['Giá bán2']=0
     eccount_gr['Ecount']="Ecount"
-    # eccount_gr
+
     return eccount_gr
 
 def push(df,str):
@@ -241,7 +265,7 @@ def push(df,str):
     # existing
     updated = existing.append(df)
     gd.set_with_dataframe(ws, updated)
-    st.success('Tải lại trang để tiếp tục nhập liệu')
+    st.success('Bấm Xóa nội dung thẻ kiện cũ để tiếp tục')
 
 
 
@@ -257,6 +281,5 @@ with c5:
         # data
         data=data[['MÃ THẺ KIỆN_2',"MÃ THẺ KIỆN","NGÀY NHẬP LIỆU","NGÀY KIỂM",	"NGƯỜI KIỂM",	"NCC",	"LOẠI GỖ",	"QC Dày",	"QC Rộng","QC Dài",	"Số thanh",	 "KHỐI LƯỢNG", 	"MÃ LÔ",'ĐỘ ẨM']]
         data=data[data["QC Dài"]>0]
-        # data
+        
         push(data,'1. NHẬP LIỆU')
-
