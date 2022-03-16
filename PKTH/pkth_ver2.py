f663aed6fb2109493ea65f4c8ccbfc3cfb5dfdb9
PKTHttf <91238030+PKTHttf@users.noreply.github.com>
2022-03-16 08:40:01 +0700

    Update pkth_ver2.py

--- PKTH/pkth_ver2.py
+++ PKTH/pkth_ver2.py
@@ -134,8 +134,8 @@ def dataset(gc1):
 import requests
 import json
 import trello
-from trello import TrelloClient
-client = TrelloClient(
+from trello import trelloclient
+client = trelloclient(
     api_key=st.secrets["api_key"],
     token=st.secrets["token"],
 )
